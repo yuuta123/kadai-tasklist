847ba0c47ae15c01378dfa4e58dafd344e646a24
yuuta.kurabuchi <yuuta-kurabuchi@ogic.co.jp>
2019-08-20 21:17:26 +0900

    Add validation

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -2,8 +2,10 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
+import models.validators.TaskValidator;
 import utils.DBUtil;
 
 /**
@@ -44,6 +47,30 @@ public class CreateServlet extends HttpServlet {
             Timestamp currentTime = new Timestamp(System.currentTimeMillis());
             t.setCreated_at(currentTime);
             t.setUpdated_at(currentTime);
+
+            // バリデーションを実行してエラーがあったら新規登録のフォームに戻る
+            List<String> errors = TaskValidator.validate(t);
+            if(errors.size() > 0) {
+                em.close();
+
+                // フォームに初期値を設定、さらにエラーメッセージを送る
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("tasks", t);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+                rd.forward(request, response);
+            } else {
+                // データベースに保存
+                em.getTransaction().begin();
+                em.persist(t);
+                em.getTransaction().commit();
+                request.getSession().setAttribute("flush", "登録が完了しました。");
+                em.close();
+
+                // indexのページにリダイレクト
+                response.sendRedirect(request.getContextPath() + "/index");
+            }
 
             // データベースに保存
             em.getTransaction().begin();
